4d742f35efcc1b7917465555111ee8cf9d33942a
friduby <friduby@gmail.com>
2022-05-03 01:36:09 +0430

    [3] 1. adding ux part 2. adding callback to json-server_attachData 3. rename files

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-import {jsonServer_attachData} from "./modules/json-server.js";
-import {UI_addP} from "./modules/cmd-butt.js";
+import { jsonServer_attachData } from "./modules/json-server.js";
+import { UX_Load } from "./modules/UX.js";
+import { UI_addP } from "./modules/UI_cmd-butt.js";
 
 
-// Load data to web-preview
-window.addEventListener('DOMContentLoaded', () => jsonServer_attachData())
+// Load data to web-preview and then UX_Load will be execute (_callback of jsonServer_attachData)
+window.addEventListener('DOMContentLoaded', () => jsonServer_attachData(UX_Load))
 
 
 // add functionality to add p button (cmd-butt-p)
 var cmdButtP = document.getElementById('cmd-butt-p');
-
 cmdButtP.addEventListener("click", UI_addP);
-
-
-// add some interactivity (selecting view)
-// document.querySelectorAll("#web-preview > *")[0].childNodes.forEach(element => {element.addEventListener('click', UX_selecting())})
--- modules/json-server.js
+++ modules/json-server.js
@@ -6,45 +6,47 @@ function documentBuilder(element, post) {
     let document = "";
     let innerDocument = "";
 
-    if(element.tag) {
-        if(Array.isArray(element.child)){element.child.sort(function(a,b){return a.siblingIndex - b.siblingIndex}).forEach(element => {innerDocument += documentBuilder(post[element.id - 1],post)});}
-        else{innerDocument += element.child;}   
-    
-            document += `<${element.tag} ${element.attribute}>${innerDocument}</${element.tag}>`
-    
-        return document;    
+    if (element.tag) {
+        if (Array.isArray(element.child)) { element.child.sort(function (a, b) { return a.siblingIndex - b.siblingIndex }).forEach(element => { innerDocument += documentBuilder(post[element.id - 1], post) }); }
+        else { innerDocument += element.child; }
+
+        document += `<${element.tag} ${element.attribute}>${innerDocument}</${element.tag}>`
+
+        return document;
     }
 
-    else{
+    else {
         document = element.child
         return document;
     }
-    
+
 }
 
 
-export const jsonServer_attachData = async () => {
+export const jsonServer_attachData = async (_callback) => {
 
     const webPreview = document.getElementById("web-preview")
 
-    const uri ='http://localhost:3000/HTMLtags';
+    const uri = 'http://localhost:3000/HTMLtags';
 
     const res = await fetch(uri);
     const post = await res.json();
 
-    post.sort(function(a,b){return a.id - b.id})
+    post.sort(function (a, b) { return a.id - b.id })
 
     let DataWebPreview = ""
 
     let roots = [];
 
-    post.forEach(element => {if(element.parent === 0){roots.push(element)}})
+    post.forEach(element => { if (element.parent === 0) { roots.push(element) } })
 
-    roots.sort(function(a,b){return a.siblingIndex - b.siblingIndex}).forEach(element => {
+    roots.sort(function (a, b) { return a.siblingIndex - b.siblingIndex }).forEach(element => {
 
         DataWebPreview += documentBuilder(element, post)
 
     });
 
     webPreview.innerHTML = DataWebPreview;
+
+    _callback()
 }
