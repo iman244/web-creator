37608d320ddcb1387ded9626b92a4e8797f83994
friduby <friduby@gmail.com>
2022-05-22 05:14:29 +0430

    modal for customizing USER prefrence start

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ import { callbacks } from "./modules/callback.js"
 import { jsonServer_saveData } from "./modules/json-server/json-server-save.js";
 import { UI_Speech_Load } from "./modules/UI/UI_speech.js";
 import { UI_elementInformation_Load } from "./modules/UI/UI_elementInformation_Load/UI_elementInformation_Load.js";
+import { modal_USER_customization_Load } from "./modules/USER/USER_customization/USER_customization.js";
 
 /*
 Read me
-
+ 
 We add functionality to four button that we have for interactivity of user.
 Then, USER_data.json will loaded to div#web-preview. Functionalities that need
 USER data to run have added to callbacks (in callback.js) to run after USER data
@@ -27,7 +28,8 @@ let HTML_jsonServer_saveData = document.getElementById('jsonServer_saveData'); H
 let HTML_Test_Load = document.getElementById('UI_TEST_Load'); HTML_Test_Load.addEventListener('click', UI_TEST_Load);
 export let HTML_Speech_Button = document.getElementById('UI_Speech_Load'); HTML_Speech_Button.addEventListener('click', UI_Speech_Load);
 
-UI_elementInformation_Load()
+UI_elementInformation_Load();
+modal_USER_customization_Load();
 
 // Load data to web-preview and then UX_Load will be execute (_callback of jsonServer_attachData)
 window.addEventListener('DOMContentLoaded', () => jsonServer_loadData(callbacks));
